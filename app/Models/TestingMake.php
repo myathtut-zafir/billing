209cb7ab42ccacae6a205f47b2a537dc64f4f0f5
myathtut-zafir <aa.htut@gmail.com>
2024-09-27 10:41:43 +0700

    feat: testing make method

---
+++ app/Models/TestingMake.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+class TestingMake
+{
+
+
+    private string $name;
+    private string $email;
+
+    public function __construct(string $name, string $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public static function make(string $name, string $email): TestingMake
+    {
+        return new self($name, $email);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+
+}
